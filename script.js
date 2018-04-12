f96382360626e18efc66be6119c57f1220eaec16
jasonszhao <jason@codesmoothie.com>
2018-04-11 23:38:16 -0700

    initial commit. add some elm architecture in front-end and a working server in the backend

---
+++ script.js
@@ -0,0 +1,100 @@
+"use strict";
+
+/***** Some "functional" utilities ****/
+const produce = immer.default.bind(immer)
+
+const log = (...args) => (
+  console.log.apply(console, args), args[args.length - 1]
+);
+
+const add = R.curry((b, a) => a + b)
+const subtract = R.curry((b, a) => a - b)
+
+const add1 = add(1)
+const subtract1 = subtract(1)
+//////// END functional utilities
+
+// Model
+const init = () => ({
+	x: 0,
+	y: 0,
+})
+
+const sum = flyd.combine(
+  (model) => model.x + model.y
+)
+
+// Actions
+const INCREMENT_X = 'increment_x';
+const DECREMENT_X = 'decrement_x';
+const INCREMENT_Y = 'increment_y';
+const DECREMENT_Y = 'decrement_y';
+
+
+function update(action, model) {
+  switch (action) {
+    case INCREMENT_X:
+			return produce(model, d => {
+				d.x += 1
+			})
+			//return {
+				//x: model.x - 1,
+				//y: model.y
+			//}
+		case DECREMENT_X:
+			return {
+				x: model.x - 1,
+				y: model.y
+			}
+			break;
+		case INCREMENT_Y:
+			return {
+				x: model.x,
+				y: model.y + 1
+			}
+			break;
+		case DECREMENT_Y:
+			return {
+				x: model.x,
+				y: model.y - 1
+			}
+			break;
+	}
+}
+const restoreState = () => {
+		const restored = JSON.parse(localStorage.getItem('state'));
+		return restored === null ? init() : restored;
+};
+const saveState = (model) => {
+  localStorage.setItem('state', JSON.stringify(model));
+};
+
+
+// View
+;const $x = document.getElementById('x');
+const $y = document.getElementById('y')
+const $sum = document.getElementById('sum')
+
+function render(model) {
+	$x.textContent = model.x
+	$y.textContent = model.y
+	$sum.textContent = model.x + model.y;
+
+	$x.style.backgroundColor = `hsl(${model.x * 10 % 360}, 100%, 50%)`;
+	$y.style.backgroundColor = `hsl(${model.y * 10 % 360}, 100%, 50%)`;
+	$sum.style.backgroundColor = `hsl(${(model.x + model.y) * 10 % 360}, 100%, 50%)`;
+}
+
+
+// Streams
+const actions = flyd.stream();
+const model = flyd.scan(R.flip(update), restoreState(), actions)
+actions
+	.map(log)
+model
+	.map(log)
+	.map(render)
+
+model
+	.map(saveState)
+
---
+++ server.js
@@ -0,0 +1,62 @@
+const util = require("util");
+
+const express = require("express");
+const axios = require("axios");
+const FeedParser = require("feedparser");
+
+const app = express();
+
+async function fetch(feed) {
+  const res = await axios.get(feed, {
+    timeout: 10000,
+    responseType: "stream",
+
+    // Some feeds do not respond without user-agent and accept headers.
+    headers: {
+      "user-agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
+      accept: "text/html,application/xhtml+xml"
+    }
+  });
+
+  const result = { articles: [] };
+
+  return new Promise((resolve, reject) => {
+    res.data
+      .pipe(new FeedParser())
+      .on("error", e => {
+        console.log(
+          "This is the response data: ",
+          typeof res.data,
+          util.inspect(res.data, { depth: 5 })
+        ),
+          result;
+        throw Error(e);
+      })
+      .on("meta", meta => (result.meta = meta))
+      .on("readable", function() {
+        let article;
+        while ((article = this.read())) {
+          result.articles.push(article);
+        }
+      })
+      .on("end", () => resolve(result));
+  });
+}
+app.get(
+  "/api/rssparser",
+
+  (req, res) =>
+    fetch(req.query.url)
+      .then(parsed => res.json(parsed))
+      .catch(e => {
+        console.log(e),
+          res.status(500).json({
+            error: "Could not get or parse feed.",
+            url: req.query.url
+          });
+      })
+);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
