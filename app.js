47582dd8e05283f5454183a3a6b7467324cb9b88
jasonszhao <jason@codesmoothie.com>
2018-04-21 17:43:29 -0700

    format app.js

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ const subtract1 = subtract(1)
 
 // Model
 const init = () => ({
-	x: 0,
-	y: 0,
+  x: 0,
+  y: 0,
 })
 
 const sum = flyd.combine(
@@ -31,39 +31,40 @@ const INCREMENT_Y = 'increment_y';
 const DECREMENT_Y = 'decrement_y';
 
 
+
 function update(action, model) {
   switch (action) {
     case INCREMENT_X:
-			return produce(model, d => {
-				d.x += 1
-			})
-			//return {
-				//x: model.x - 1,
-				//y: model.y
-			//}
-		case DECREMENT_X:
-			return {
-				x: model.x - 1,
-				y: model.y
-			}
-			break;
-		case INCREMENT_Y:
-			return {
-				x: model.x,
-				y: model.y + 1
-			}
-			break;
-		case DECREMENT_Y:
-			return {
-				x: model.x,
-				y: model.y - 1
-			}
-			break;
-	}
+      return produce(model, d => {
+	d.x += 1
+      })
+      //return {
+      //x: model.x - 1,
+      //y: model.y
+      //}
+    case DECREMENT_X:
+      return {
+	x: model.x - 1,
+	y: model.y
+      }
+      break;
+    case INCREMENT_Y:
+      return {
+	x: model.x,
+	y: model.y + 1
+      }
+      break;
+    case DECREMENT_Y:
+      return {
+	x: model.x,
+	y: model.y - 1
+      }
+      break;
+  }
 }
 const restoreState = () => {
-		const restored = JSON.parse(localStorage.getItem('state'));
-		return restored === null ? init() : restored;
+  const restored = JSON.parse(localStorage.getItem('state'));
+  return restored === null ? init() : restored;
 };
 const saveState = (model) => {
   localStorage.setItem('state', JSON.stringify(model));
@@ -71,18 +72,18 @@ const saveState = (model) => {
 
 
 // View
-;const $x = document.getElementById('x');
+const $x = document.getElementById('x');
 const $y = document.getElementById('y')
 const $sum = document.getElementById('sum')
 
 function render(model) {
-	$x.textContent = model.x
-	$y.textContent = model.y
-	$sum.textContent = model.x + model.y;
+  $x.textContent = model.x
+  $y.textContent = model.y
+  $sum.textContent = model.x + model.y;
 
-	$x.style.backgroundColor = `hsl(${model.x * 10 % 360}, 100%, 50%)`;
-	$y.style.backgroundColor = `hsl(${model.y * 10 % 360}, 100%, 50%)`;
-	$sum.style.backgroundColor = `hsl(${(model.x + model.y) * 10 % 360}, 100%, 50%)`;
+  $x.style.backgroundColor = `hsl(${model.x * 10 % 360}, 100%, 50%)`;
+  $y.style.backgroundColor = `hsl(${model.y * 10 % 360}, 100%, 50%)`;
+  $sum.style.backgroundColor = `hsl(${(model.x + model.y) * 10 % 360}, 100%, 50%)`;
 }
 
 
@@ -90,11 +91,11 @@ function render(model) {
 const actions = flyd.stream();
 const model = flyd.scan(R.flip(update), restoreState(), actions)
 actions
-	.map(log)
+  .map(log)
 model
-	.map(log)
-	.map(render)
+  .map(log)
+  .map(render)
 
 model
-	.map(saveState)
+  .map(saveState)
 
