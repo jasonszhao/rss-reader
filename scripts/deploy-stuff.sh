3f42633fbd0e4d47ba2bd5b1c7ebf6a75b3df802
jasonszhao <jason@codesmoothie.com>
2019-03-15 20:39:01 -0700

    fix deployment scripts

--- scripts/deploy-stuff.sh
+++ scripts/deploy-stuff.sh
@@ -6,8 +6,8 @@
 git clone --depth=1 https://github.com/jasonszhao/rss-reader.git
 cd rss-reader
 
-# set up the service 
-sudo echo "
+# set up the service
+sudo tee /etc/systemd/system/rss-reader.service <<EOF
 [Unit]
 Description=runs the Node server for the RSS Reader app
 
@@ -20,11 +20,11 @@ WorkingDirectory=/home/Jason/rss-reader
 
 [Install]
 WantedBy=multi-user.target
-" >! /etc/systemd/system/rss-service.service
+EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable rss-reader.service
-sudo systemctl start rss-reader
+sudo systemctl restart rss-reader
 
 
 # port forwarding
@@ -33,5 +33,3 @@ sudo echo "post-down iptables-save > /etc/iptables.rules" >> /etc/network/interf
 
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 3000
 sudo iptables -t nat -I OUTPUT -p tcp -o lo --dport 80 -j REDIRECT --to-ports 3000
-
-
